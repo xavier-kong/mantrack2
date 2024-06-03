38db0c0252e0bd6825657f15ea2894c64efbea93
xavier-kong <kongxianchengxavier@gmail.com>
2024-06-03 08:56:20 +0800

    some routers

--- src/server/api/drizzle/schema.ts
+++ src/server/api/drizzle/schema.ts
@@ -1,7 +1,11 @@
 import { sql } from "drizzle-orm";
 import { text, sqliteTable } from "drizzle-orm/sqlite-core";
 
-const foo = sqliteTable("foo", {
-    bar: text("bar").notNull().default("Hey!"),
-});
+export const userTable = sqliteTable("user", {
+    username: text("username").unique().notNull().primaryKey(),
+    password: text("password").notNull()
+})
 
+export const entriesTable = sqliteTable("entries", {
+    username: text("username").notNull(),
+})
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,10 +2,72 @@ import { z } from "zod";
 import { db } from '../db';
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { userTable } from "../drizzle/schema";
+import { eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
 
 export const mainRouter = createTRPCRouter({
+  createUser: publicProcedure
+  .input(z.object({ username: z.string(), password: z.string() }))
+  .mutation(async ({ input }) => {
+    const userQuery = await db.select().from(userTable).where(eq(userTable.username, input.username));
+
+    if (userQuery.length !== 0) {
+      throw new TRPCError({ 
+        code: 'BAD_REQUEST',
+        message: 'username already taken'
+      });
+    }
+
+    try {
+      await db.insert(userTable).values(input);
+
+      return {
+        success: true
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        success: false
+      };
+    }
+  }),
+  login: publicProcedure
+  .input(z.object({ username: z.string(), password: z.string() }))
+  .mutation(async ({ input }) => {
+    await validateUserCreds({ ...input });
+    return { success: true };
+  }),
+  getUserdata: publicProcedure
+  .input(z.object({ username: z.string(), password: z.string() }))
+  .query(async ({ input }) => {
+    await validateUserCreds({ ...input });
+
+    const dataQuery = await db.select().from(entriesTable).where(eq(entriesTable.username, username));
+
+  })
 
 })
+
+async function validateUserCreds({ username, password }: { username: string; password: string }) {
+  const userQuery = await db.select().from(userTable).where(eq(userTable.username, username));
+
+  if (userQuery.length === 0) {
+    throw new TRPCError({ 
+      code: 'BAD_REQUEST',
+      message: 'user not found'
+    });
+  }
+
+  const userPassword = userQuery[0]?.password;
+
+  if (userPassword !== password) {
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'username or password invalid'
+    })
+  }
+}
 
 
 /*
